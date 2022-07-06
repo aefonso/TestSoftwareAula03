c9af2932167db1d1645509dd217a2d5c995afc78
Afonso Henrique <hafonso021@gmail.com>
2022-07-06 08:20:56 -0300

    feat(dobro): Update

--- src/pages/Dobro.js
+++ src/pages/Dobro.js
@@ -3,13 +3,7 @@ import { Text, View, Button } from "react-native";
 export default props => {
     return (
         <View>
-            <Text>Texto Inicial</Text>
-            <Button
-            title="Ir para Index"
-            onPress={() => {
-                props.navigation.navigate("Index");
-            }}
-            />
+            <Text>Texto Dobro</Text>
         </View>
     )
 }
